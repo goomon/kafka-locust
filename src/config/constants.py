6be7e28aada572f686d471c3e8031d2b9635df08
goomon <jaegoo.jho@gmail.com>
2023-04-26 14:12:53 +0900

    Chore: remove unnecessary constant.

--- src/config/constants.py
+++ src/config/constants.py
@@ -4,6 +4,3 @@ import os
 SAMPLING_RATE = 700
 WINDOW_SIZE = 2 if os.getenv("WINDOW_SIZE") is None else int(os.getenv("WINDOW_SIZE"))
 OVERLAP_RATIO = 0.5 if os.getenv("OVERLAP_RATIO") is None else float(os.getenv("OVERLAP_RATIO"))
-
-# Topic
-MAIN_TOPIC = "data"
