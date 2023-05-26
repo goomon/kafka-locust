b36efaf1ad77cd172dd903b192ae468e5d5153b8
goomon <jaegoo.jho@gmail.com>
2023-05-26 15:39:36 +0900

    Feat: simulation without middleware server.

--- src/locustfile.py
+++ src/locustfile.py
@@ -1,13 +1,12 @@
 import time
 import uuid
+import json
 
-from locust import task, HttpUser, events
+from confluent_kafka import Producer
+from locust import task, User, events
 from sqlalchemy.orm import Session
 
 from mock_generator import MockSensorDataGenerator
-from db import models
-from db.models import Base
-from db.database import engine, get_db
 
 session: Session = None
 
@@ -15,20 +14,21 @@ session: Session = None
 @events.test_start.add_listener
 def on_test_start(environment, **kwargs):
     global session
-    Base.metadata.create_all(engine)
-    session = get_db()
+    # Base.metadata.create_all(engine)
+    # session = get_db()
 
 
 @events.test_stop.add_listener
 def on_test_stop(environment, **kwargs):
     global session
-    session.commit()
-    session.close()
+    # session.commit()
+    # session.close()
     print("test stop!");
 
 
-class SpringLocust(HttpUser):
-    client = None
+class KafkaLocust(User):
+    conf = {"bootstrap.servers": "localhost:19092"}
+    producer = Producer(conf)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -42,14 +42,16 @@ class SpringLocust(HttpUser):
 
     @task
     def send_data(self):
-        global session
-        msg = self.generator.generate_data()
-        if msg is not None:
-            self.client.post("/", json=msg)
-            data = models.StartRecord(
-                connection_id=msg["connection_id"],
-                timestamp=msg["timestamp"],
-                response_time=int(time.time()*1000)
-            )
-            session.add(data)
+        data = self.generator.generate_data()
+        if data is not None:
+            try:
+                self.producer.produce(
+                    topic="chest",
+                    value=json.dumps(data),
+                    key=self.user_id,
+                )
+                self.producer.flush()
+            except RuntimeError as e:
+                pass
+            print("success!")
         time.sleep(1)
